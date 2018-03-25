fa7a7136ab20ca42e3d3dee24dbd1dadf8e24f14
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2018-03-25 14:02:00 +0200

    Add simple result-file comparison

--- git_history_profiler/main.py
+++ git_history_profiler/main.py
@@ -31,6 +31,11 @@ class Repository:
         return os.path.abspath(os.path.join(
             self.config['working_directory'], 'repo'))
 
+    @property
+    def results_dir(self) -> str:
+        return os.path.abspath(os.path.join(
+            self.config['working_directory'], 'result_cache'))
+
     def clone(self) -> None:
         if os.path.exists(self.url):
             # is local
@@ -52,24 +57,36 @@ class Repository:
         cmd_path = os.path.join(self.config_dir, self.config['init_script'])
         os.system(f'{cmd_path} "{self.repo_dir}" > /dev/null')
 
-    def execute(self) -> List:
+    def execute(self, commit_id: str) -> List:
         stats = []
         for job in tqdm(self.config['jobs'], desc='Jobs'):
             cmd_path = os.path.join(self.config_dir, job['command'])
             # cmd = sh.Command(cmd_path)
 
+            # time command
             start = time.time()
             # cmd(_cwd=self.repo_dir)
             os.system(f'cd "{self.repo_dir}" && {cmd_path} > /dev/null')
             dur = time.time() - start
 
-            stats.append((job['name'], dur))
+            # handle results
+            result_files = []
+            result_cache = os.path.join(
+                self.results_dir, commit_id, job['name'].replace(' ', '_'))
+            os.makedirs(result_cache)
+            for fname in job['files']:
+                fpath = os.path.join(self.repo_dir, fname)
+                shutil.copy(fpath, result_cache)
+                result_files.append(
+                    os.path.join(result_cache, fname))
+
+            stats.append((commit_id, job['name'], dur, result_files))
         return stats
 
     def handle_commit(self, commit_id: str) -> List:
         self.clean()
         self.switch_to_commit(commit_id)
-        return self.execute()
+        return self.execute(commit_id)
 
     def list_commits(self) -> List[str]:
         buf = StringIO()
@@ -102,21 +119,25 @@ class Repository:
 
         for commit in tqdm(commits, desc='Commit history'):
             res = self.handle_commit(commit)
-            stats.append((commit, res))
-        return stats
+            stats.extend(res)
 
-    def plot(self, data: List) -> None:
-        # transform data
-        tmp = []
-        for commit, jobs in data:
-            for name, duration in jobs:
-                tmp.append((commit[:7], name, duration))
-        df = pd.DataFrame(tmp, columns=['commit', 'job', 'time'])
+        df = pd.DataFrame(
+            stats,
+            columns=['commit', 'job', 'time', 'result_files'])
+        df.to_csv(
+            os.path.join(self.config['working_directory'], 'raw.csv'),
+            index=False)
+        return df
 
+    def plot(self, df: pd.DataFrame) -> None:
         # plot
+        plt.figure()
         sns.pointplot(x='commit', y='time', hue='job', data=df)
 
         plt.xticks(rotation=90)
+        plt.gca().set_xticklabels(
+            [item.get_text()[:7] for item in plt.gca().get_xticklabels()])
+
         plt.xlabel('Commits [id]')
         plt.ylabel('Execution time [s]')
         plt.title('Performance overview')
@@ -124,6 +145,31 @@ class Repository:
         plt.tight_layout()
         plt.savefig(os.path.join(
             self.config['working_directory'], 'performance.pdf'))
+
+    def compare_results(self, df: pd.DataFrame) -> None:
+        # gather data
+        data = []
+        for row in df.itertuples():
+            for fpath in row.result_files:
+                fsize = os.path.getsize(fpath)
+                data.append((row.commit, os.path.basename(fpath), fsize))
+        df = pd.DataFrame(data, columns=['commit', 'result_file', 'size'])
+
+        # plot result
+        plt.figure()
+        sns.pointplot(x='commit', y='size', hue='result_file', data=df)
+
+        plt.xticks(rotation=90)
+        plt.gca().set_xticklabels(
+            [item.get_text()[:7] for item in plt.gca().get_xticklabels()])
+
+        plt.xlabel('Commits [id]')
+        plt.ylabel('File size [bytes]')
+        plt.title('Result file overview')
+
+        plt.tight_layout()
+        plt.savefig(os.path.join(
+            self.config['working_directory'], 'result_overview.pdf'))
 
 
 @click.command()
@@ -139,9 +185,10 @@ def main(repo_url: str, config: str, commit: List[str]) -> None:
     """ Performance and stability profiling over the git commit history.
     """
     repo = Repository(repo_url, config)
+    df = repo.run(commit if len(commit) > 0 else None)
 
-    res = repo.run(commit if len(commit) > 0 else None)
-    repo.plot(res)
+    repo.plot(df)
+    repo.compare_results(df)
 
 
 if __name__ == '__main__':
